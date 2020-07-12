69be8317b0b23d18b6c74a730df2139d0dcd8ade
vandana <vandu565@gmail.com>
2020-07-12 18:50:17 +0530

    post and comments done

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,48 @@
-module.exports.home=function(req,res){
-    console.log(req.cookies);
-    res.cookie('user_id',25);
-    return res.render('home',{
-        title:"home"
-    });
+//module.exports.home=function(req,res){
+   // console.log(req.cookies);
+   // res.cookie('user_id',25);
+   // return res.render('home',{
+       // title:"home"
+   // });
+//}
+
+const Post=require('../models/post');
+
+//module.exports.home=function(req,res){
+    
+    //Post.find({},function(err,posts){
+       // return res.render('home',{
+            // title:"Codeal/Home",
+            // posts:posts
+         //});
+
+    //});
+
+      // populate the user of each post
+    // Post.find({}).populate('user').exec(function(err, posts){
+       // return res.render('home', {
+       //     title: "Codeial | Home",
+        //    posts:  posts
+       // });
+   // })
+//}
+
+
+module.exports.home = function(req, res){
+    
+    Post.find({})
+    .populate('user')
+    .populate({
+        path: 'comments',
+        populate: {
+            path: 'user'
+        }
+    })
+    .exec(function(err, posts){
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts:  posts
+        });
+    })
+
 }
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,8 @@ console.log('router loaded');
 router.get('/',homeController.home);
 
 router.use('/users',require('./users'));
-
+//router.use('/posts',require('./posts'));
+router.use('/posts', require('./posts'));
+router.use('/comments', require('./comments'));
 
 module.exports=router;
